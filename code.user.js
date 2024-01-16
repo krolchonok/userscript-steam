e0b2a606fa35a8316c891e4ec9ab379538fb1de5
ushastoe <fedosey8@gmail.com>
2024-01-16 15:57:36 +0300

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/krolchonok
 // @author      krolchonok
 // @license     MIT
-// @version     1.0.2
+// @version     1.0.3
 // @description Enhances the Steam Inventory and Steam Market.
 // @match     *://steamcommunity.com/id/*/inventory*
 // @match     *://steamcommunity.com/profiles/*/inventory*
